a85e2478bee495c0bd1d2cb66e29bcecd59eecfe
垣内勇人 <hayatokakiuchi@kakiuchihayatonomacbook-pro.local>
2019-02-27 21:45:47 +0900

    Initial Commit

---
+++ Image,Text,Alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image,Text,Alert
+//
+//  Created by 垣内勇人 on 2019/02/27.
+//  Copyright © 2019 Hayatokakiuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Image,Text,AlertTests/Image_Text_AlertTests.swift
@@ -0,0 +1,34 @@
+//
+//  Image_Text_AlertTests.swift
+//  Image,Text,AlertTests
+//
+//  Created by 垣内勇人 on 2019/02/27.
+//  Copyright © 2019 Hayatokakiuchi. All rights reserved.
+//
+
+import XCTest
+@testable import Image_Text_Alert
+
+class Image_Text_AlertTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
